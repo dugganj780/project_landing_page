d2b405ce292d93a8b0523983e3b4d96cd11b4209
dugganj780 <dugganj780@gmail.com>
2022-04-03 14:19:05 +0100

    Test2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Paper from "@mui/material/Paper";
+import { makeStyles } from "@mui/styles";
+
+const useStyles = makeStyles({
+  paper: {
+    backgroundColor: "red",
+    width: "80%",
+    height: "80%",
+  },
+});
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const classes = useStyles();
+
+  return <Paper className={classes.paper}></Paper>;
 }
 
 export default App;
